46e3467d83987787377b95c088fa36bd6f898e94
E-Kosterlits <estherkosterlits@gmail.com>
2026-01-04 17:25:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const hours= document.getElementById("hours");
 const minutes= document.getElementById("minutes");
 const seconds= document.getElementById("seconds");
 
-const weddingDate= new Date("October 30 2025 19:30:00").getTime();
+
+const weddingDate= new Date("October 31 2025 00:00:00").getTime();
 
 function timer() {
     const currentDate= new Date().getTime();
@@ -14,11 +15,18 @@ function timer() {
     const Minutes= Math.floor(distance/1000/60)%60;
     const Seconds= Math.floor(distance/1000)%60;
 
-    days.innerHTML=Days;
-    hours.innerHTML=Hours;
-    minutes.innerHTML=Minutes;
-    seconds.innerHTML=Seconds;
+    days.innerHTML=Math.abs(Days);
+    hours.innerHTML=Math.abs(Hours);
+    minutes.innerHTML=Math.abs(Minutes);
+    seconds.innerHTML=Math.abs(Seconds);
 
+    const Title= document.getElementById("title");
+if (Days < 0){
+    Title.innerHTML="the Wedding was this long ago:";
+    document.body.style.backgroundImage= "url(BGwedding.jpg)";
+
+}
 }
 
 setInterval(timer, 1000);
+
